0e7414021fddedadc6faa360a8f56bfa91eab307
Rahat <rahatahmedbosscomputer@gmail.com>
2025-02-04 21:24:04 +0600

    mobile view media and fix search icon color isue

--- script/script.js
+++ script/script.js
@@ -1,21 +1,26 @@
 let navbar=document.querySelector('header');
+const searchIcon = document.querySelector('.search-icon-box a');
 
 if (window.scrollY===0){
     navbar.classList.remove('nav-container');
     navbar.classList.add('nav-top-container');
+    searchIcon.style.color = '#636363';
 }
 else{
     navbar.classList.remove('nav-top-container');
     navbar.classList.add('nav-container');
+    searchIcon.style.color = 'black';
 }
 
 window.addEventListener('scroll',function() {
     if (window.scrollY===0){
         navbar.classList.remove('nav-container');
         navbar.classList.add('nav-top-container');
+        searchIcon.style.color = '#636363';
     }
     else{
         navbar.classList.remove('nav-top-container');
         navbar.classList.add('nav-container');
+        searchIcon.style.color = 'black';
     }
 })

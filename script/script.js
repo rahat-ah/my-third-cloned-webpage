8e98620c196a4a76b40e9555b5635c6db7c030b8
Rahat <rahatahmedbosscomputer@gmail.com>
2025-02-04 20:45:55 +0600

    fix the navbar sliding by scroll isue with diffefent classlist and js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,21 @@
-window.addEventListener('scroll',function() {
-    let navbar=document.querySelector('header');
-    let navbarAtag = document.querySelectorAll('nav ul li a');
+let navbar=document.querySelector('header');
 
+if (window.scrollY===0){
+    navbar.classList.remove('nav-container');
+    navbar.classList.add('nav-top-container');
+}
+else{
+    navbar.classList.remove('nav-top-container');
+    navbar.classList.add('nav-container');
+}
+
+window.addEventListener('scroll',function() {
+    if (window.scrollY===0){
+        navbar.classList.remove('nav-container');
+        navbar.classList.add('nav-top-container');
+    }
+    else{
+        navbar.classList.remove('nav-top-container');
+        navbar.classList.add('nav-container');
+    }
 })
